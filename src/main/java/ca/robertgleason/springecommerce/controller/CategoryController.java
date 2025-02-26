dea92a878dda3db13c0ef3aba2c6175d73d0d85a
Robert Gleason <l2ob122@gmail.com>
2025-02-25 20:53:06 -0800

    implement CategoryService and update CategoryController to use it

--- src/main/java/ca/robertgleason/springecommerce/controller/CategoryController.java
+++ src/main/java/ca/robertgleason/springecommerce/controller/CategoryController.java
@@ -1,31 +1,34 @@
 package ca.robertgleason.springecommerce.controller;
 
-
 import ca.robertgleason.springecommerce.model.Category;
+import ca.robertgleason.springecommerce.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
+
 
 @RestController
 public class CategoryController {
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
-
-    private List<Category> categories = new ArrayList<>();
+    private CategoryService categoryService;
 
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
 
     @PostMapping("/api/public/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "Category Added successfully";
     }
 }
---
+++ src/main/java/ca/robertgleason/springecommerce/service/CategoryService.java
@@ -0,0 +1,13 @@
+package ca.robertgleason.springecommerce.service;
+
+import ca.robertgleason.springecommerce.model.Category;
+
+import java.util.List;
+
+
+public interface CategoryService {
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+
+}
