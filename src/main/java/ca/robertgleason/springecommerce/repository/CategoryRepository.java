a0b868f86a8f545ed92072b208be79a973d6c996
Robert Gleason <l2ob122@gmail.com>
2025-02-26 19:40:07 -0800

    implement CategoryRepository and update CategoryServiceImpl to use JPA for data persistence; enhance application properties for H2 database configuration

---
+++ src/main/java/ca/robertgleason/springecommerce/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package ca.robertgleason.springecommerce.repository;
+
+
+import ca.robertgleason.springecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+   
+}
--- src/main/java/ca/robertgleason/springecommerce/service/CategoryServiceImpl.java
+++ src/main/java/ca/robertgleason/springecommerce/service/CategoryServiceImpl.java
@@ -2,32 +2,39 @@ package ca.robertgleason.springecommerce.service;
 
 
 import ca.robertgleason.springecommerce.model.Category;
+import ca.robertgleason.springecommerce.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1L;
+    //    private List<Category> categories = new ArrayList<>();
+//    private Long nextId = 1L;
+
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+//        category.setCategoryId(nextId++);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
+        List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
@@ -38,14 +45,14 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
 
-        categories.remove(category);
+        categoryRepository.deleteById(categoryId);
 
         return "Category with category Id: " + categoryId + " deleted successfully";
     }
 
     @Override
     public String updateCategory(Long categoryId, Category category) {
-        Category categoryToUpdate = categories.stream()
+        Category categoryToUpdate = categoryRepository.findAll().stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "Resource Not Found"));
@@ -55,6 +62,7 @@ public class CategoryServiceImpl implements CategoryService {
         }
 
         categoryToUpdate.setCategoryName(category.getCategoryName());
+        categoryRepository.save(categoryToUpdate);
 
         return "Category with category Id: " + categoryId + " updated successfully";
     }
